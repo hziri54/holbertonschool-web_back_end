1b6df26ffcaf5224e89c0e8dea3f203282ceaaea
Ziri Houamdi <zirihouamdi@MacBook-Air-de-Ziri.local>
2024-07-18 14:38:59 +0200

    error space task 0

--- ES6_basic/0-constants.js
+++ ES6_basic/0-constants.js
@@ -1,5 +1,15 @@
-export default function taskBlock() {
-    const task = false;
-    const task2 = true;
-    return [task, task2];
-  }
+export function taskFirst() {
+  const task = 'I prefer const when I can.';
+  return task;
+}
+
+export function getLast() {
+  return ' is okay';
+}
+
+export function taskNext() {
+  let combination = 'But sometimes let';
+  combination += getLast();
+
+  return combination;
+}
