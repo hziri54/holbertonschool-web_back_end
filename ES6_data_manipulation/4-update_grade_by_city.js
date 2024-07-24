9775b9550950d3137b55a2c7e3f5794a022a6623
Ziri Houamdi <zirihouamdi@MacBook-Air-de-Ziri.local>
2024-07-24 17:31:45 +0200

    debug task 4

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,8 +1,8 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
     return students
       .filter((student) => student.location === city)
-      .map((student) => {
-        const gradeId = newGrades
+       .map((student) => {
+         const gradeId = newGrades
           .findIndex((grade) => grade.studentId === student.id);
         return {
           ...student,
