8509d9a4bc87bab906286a361883f4bdf0ba821d
Ziri Houamdi <zirihouamdi@air-de-ziri.home>
2024-07-24 13:31:27 +0200

    modif eslinc

---
+++ ES6_promise/.eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    env: {
+      node: true, // Ajoute les variables globales pour Node.js
+      es6: true,  // Ajoute les variables globales pour ES6
+    },
+    // autres configurations
+  };
+  
