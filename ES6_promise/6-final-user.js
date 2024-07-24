01055901c02263d973edf246c59d9008fbbe2c53
Ziri Houamdi <zirihouamdi@MacBook-Air-de-Ziri.local>
2024-07-24 16:28:27 +0200

    task 6 finish

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,11 +1,10 @@
-  import signUpUser from './4-user-promise';
-  import uploadPhoto from './5-photo-reject';
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
-  export default function handleProfileSignup(firstName, lastName, fileName) {
+export default function handleProfileSignup(firstName, lastName, fileName) {
   const res = Promise.all([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
   return res.map((res) => ({
-  status: res.status,
-  body: res.status === 'success' ? res.value : $(`Error: ${res.value.message}`),
+    status: res.status,
+    body: res.status === 'success' ? res.value : $(`Error: ${res.value.message}`),
   }));
-  }
-  
+}
