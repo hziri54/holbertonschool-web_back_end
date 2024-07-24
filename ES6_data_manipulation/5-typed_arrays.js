d2c60734db87a5e6629f7b5c422b7e82fff5fead
Ziri Houamdi <zirihouamdi@MacBook-Air-de-Ziri.local>
2024-07-24 17:48:07 +0200

    task 5 debbug

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,9 +1,9 @@
 export default function createInt8TypedArray(length, position, value) {
-    if (position < 0 || position >= length) {
-      throw new RangeError('Position outside the array');
-    }
-    const array = new ArrayBuffer(length);
-    const view = new DataView(array);
-    view.setInt8(position, value);
-    return view;
+  if (position < 0 || position >= length) {
+    throw new RangeError('Position outside the array');
+  }
+  const array = new ArrayBuffer(length);
+  const view = new DataView(array);
+  view.setInt8(position, value);
+  return view;
 }
