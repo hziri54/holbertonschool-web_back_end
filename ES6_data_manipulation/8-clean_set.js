60afc192f82566deb01784e67368b5dd1d7f6fcc
Ziri Houamdi <zirihouamdi@MacBook-Air-de-Ziri.local>
2024-07-24 11:30:12 +0200

    task ! FINISH

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,14 @@
+export default function cleanSet(set, startString) {
+    if (startString === '' || typeof startString !== 'string') {
+      return '';
+    }
+    const res = [];
+  
+    for (const item of set) {
+      if (item.startsWith(startString)) {
+        res.push(item.slice(startString.length));
+      }
+    }
+    const result = [...new Set(res)];
+    return result.join('-');
+  }
