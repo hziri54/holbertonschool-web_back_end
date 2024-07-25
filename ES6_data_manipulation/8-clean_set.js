42d7d9f73a58389397e56ca58b898d0fb4f86405
Ziri Houamdi <zirihouamdi@MacBook-Air-de-Ziri.local>
2024-07-25 10:26:34 +0200

    task 8 debbug

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,14 +1,14 @@
 export default function cleanSet(set, startString) {
-    if (startString === '' || typeof startString !== 'string') {
-      return '';
+  if (startString === '' || typeof startString !== 'string') {
+    return '';
+  }
+  const res = [];
+
+  for (const item of set) {
+    if (item.startsWith(startString)) {
+      res.push(item.slice(startString.length));
     }
-    const res = [];
-  
-    for (const item of set) {
-      if (item.startsWith(startString)) {
-        res.push(item.slice(startString.length));
-      }
-    }
-    const result = [...new Set(res)];
-    return result.join('-');
+  }
+  const result = [...new Set(res)];
+  return result.join('-');
 }
