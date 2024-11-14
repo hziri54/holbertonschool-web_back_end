202a75305ff3e2246258e361ea1100ac5f16b3bf
hziri54 <zirihouamdi6'@gmail.com>
2024-11-14 09:30:39 +0100

    task 8 finish

---
+++ ES6_basic/10-loops.js
@@ -0,0 +1,9 @@
+export default function appendToEachArrayValue(array, appendString) {
+    const NewAr = [];
+  
+    for (const idx of array) {
+      NewAr.push(appendString + idx);
+    }
+  
+    return NewAr;
+  }
--- ES6_basic/8-getBudgetCurrentYear.js
+++ ES6_basic/8-getBudgetCurrentYear.js
@@ -2,11 +2,12 @@ function getCurrentYear() {
   const date = new Date();
   return date.getFullYear();
 }
-
-export default function getBudgetForCurrentYear(income, gdp, capita) {
+function getBudgetForCurrentYear(income, gdp, capita) {
   return {
     [`income-${getCurrentYear()}`]: income,
     [`gdp-${getCurrentYear()}`]: gdp,
     [`capita-${getCurrentYear()}`]: capita,
   };
 }
+
+export { getBudgetForCurrentYear };
