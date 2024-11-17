0a2c7db5252ef9a7736929c3a6ab4924d03e6d00
hziri54 <zirihouamdi6'@gmail.com>
2024-11-17 20:27:13 +0100

    task 3 finish

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -1,26 +1,26 @@
-  export default class Currency {
-  constructor(code, name) {
-      this._code = code;
-      this._name = name;
-  }
+export default class Currency {
+constructor(code, name) {
+    this._code = code;
+    this._name = name;
+}
 
-  get name() {
-      return this._name;
-  }
+get name() {
+    return this._name;
+}
 
-  set name(name) {
-      this._name = name;
-  }
+set name(name) {
+    this._name = name;
+}
 
-  get code() {
-      return this._code;
-  }
+get code() {
+    return this._code;
+}
 
-  set code(code) {
-      this._code = code;
-  }
+set code(code) {
+    this._code = code;
+}
 
-  displayFullCurrency() {
-      return `${this._name} (${this._code})`;
-  }
-  }
+displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+}
+}
