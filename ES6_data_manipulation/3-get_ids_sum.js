24c923ec5bdba36b400a5c6d075f8cc26ddadcf9
hziri54 <zirihouamdi6'@gmail.com>
2024-11-20 10:24:51 +0100

    task 3 finish

--- ES6_data_manipulation/3-get_ids_sum.js
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -1,6 +1,7 @@
-import getListStudents from "./0-get_list_students.js";
-import getStudentsByLocation from "./2-get_students_by_loc.js";
-
-const students = getListStudents();
-
-console.log(getStudentsByLocation(students, 'San Francisco'));
+export default function getStudentIdsSum(list) {
+    if (!Array.isArray(list)) {
+      return 0;
+    }
+    return list.reduce((sum, student) => sum + student.id, 0);
+  }
+  
