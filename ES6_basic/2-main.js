c88efd85cebfa142792acb26a5ba5a5331b0ffc7
hziri54 <zirihouamdi6'@gmail.com>
2024-11-20 14:23:14 +0100

    modif project

--- ES6_basic/2-main.js
+++
@@ -1,5 +0,0 @@
-import GetNeighborhoodsList from './2-arrow';
-
-const neighborhoodsList = new GetNeighborhoodsList();
-const res = neighborhoodsList.addNeighborhood('Noe Valley');
-console.log(res);
