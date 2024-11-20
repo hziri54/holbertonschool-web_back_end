6aaf3005da98430fde605dec8143768ed0d27d11
hziri54 <zirihouamdi6'@gmail.com>
2024-11-20 09:56:51 +0100

    task 1 finish

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,7 +1,6 @@
-export default function getListStudentIds(array) {
+  export default function getListStudentIds(array) {
     if (!Array.isArray(list)) {
       return [];
     }
     return array.map((student) => student.id);
   }
-  
