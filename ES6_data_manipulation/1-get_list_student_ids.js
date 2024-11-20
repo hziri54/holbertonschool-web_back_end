8088949b6c177b7042f440caca35335c19a5b7e5
hziri54 <zirihouamdi6'@gmail.com>
2024-11-20 09:52:53 +0100

    task 1 finish

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,5 +1,7 @@
-import getListStudentIds from "./1-get_list_student_ids.js";
-import getListStudents from "./0-get_list_students.js";
-
-console.log(getListStudentIds("hello"));
-console.log(getListStudentIds(getListStudents()));
+export default function getListStudentIds(array) {
+    if (!Array.isArray(list)) {
+      return [];
+    }
+    return array.map((student) => student.id);
+  }
+  
