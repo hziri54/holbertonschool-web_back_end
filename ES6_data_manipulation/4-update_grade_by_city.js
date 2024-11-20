cbfdc64a8fc4901917eb9b73aad9c30f539e4db9
hziri54 <zirihouamdi6'@gmail.com>
2024-11-20 10:37:04 +0100

    task 4 finish?

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,15 +1,14 @@
-export default function updateStudentGradeByCity(list, city, newGrades) {
-    if (!Array.isArray(list)) {
+  export default function updateStudentGradeByCity(list, city, newGrades) {
+  if (!Array.isArray(list)) {
       return [];
-    }
-    return list
+  }
+  return list
       .filter((student) => student.location === city)
       .map((student) => {
-        const gradeObj = newGrades.find((grade) => grade.studentId === student.id);
-        return {
+      const gradeObj = newGrades.find((grade) => grade.studentId === student.id);
+      return {
           ...student,
           grade: gradeObj ? gradeObj.grade : 'N/A',
-        };
+      };
       });
   }
-  
