1bfd67b41e349e0c7c624ce21852d40e1bf7816f
hziri54 <zirihouamdi6'@gmail.com>
2024-11-21 11:14:41 +0100

    resetup

--- ES6_promise/.eslintrc.js
+++ ES6_promise/.eslintrc.js
@@ -1,35 +1,20 @@
-module.exports = {
-    env: {
-      browser: false,
-      es6: true,
-      jest: true,
+{
+    "env": {
+      "browser": true,
+      "es6": true,
+      "node": true
     },
-    extends: [
-      'airbnb-base',
-      'plugin:jest/all',
-    ],
-    globals: {
-      Atomics: 'readonly',
-      SharedArrayBuffer: 'readonly',
+    "extends": "eslint:recommended",
+    "parserOptions": {
+      "ecmaVersion": 2020,
+      "sourceType": "module"
     },
-    parserOptions: {
-      ecmaVersion: 2018,
-      sourceType: 'module',
-    },
-    plugins: ['jest'],
-    rules: {
-      'no-console': 'off',
-      'no-shadow': 'off',
-      'no-restricted-syntax': [
-        'error',
-        'LabeledStatement',
-        'WithStatement',
-      ],
-    },
-    overrides:[
-      {
-        files: ['*.js'],
-        excludedFiles: 'babel.config.js',
-      }
-    ]
-  };
+    "rules": {
+      "indent": ["error", 2],
+      "quotes": ["error", "single"],
+      "semi": ["error", "always"],
+      "no-trailing-spaces": "error",
+      "eol-last": ["error", "always"]
+    }
+  }
+  
