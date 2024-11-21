b900af2a21e156049d749d2650d922ea22a09739
hziri54 <zirihouamdi6'@gmail.com>
2024-11-21 10:22:44 +0100

    task 10 debbug

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -1,13 +1,12 @@
 export default function updateUniqueItems(map) {
-    if (!(map instanceof Map)) {
-      throw new Error('Cannot process');
-    }
-  
-    for (const [key, value] of map.entries()) {
-      if (value === 1) {
-        map.set(key, 100);
-      }
-    }
-    return map;
+  if (!(map instanceof Map)) {
+    throw new Error('Cannot process');
   }
-  
+
+  map.forEach((value, key) => {
+    if (value === 1) {
+      map.set(key, 100);
+    }
+  });
+  return map;
+}
