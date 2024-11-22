58786036111fb02a58e07850382b18e108a15b99
hziri54 <zirihouamdi6'@gmail.com>
2024-11-22 14:20:44 +0100

    supp main

--- ES6_promise/.eslintrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    env: {
-      node: true, // Ajoute les variables globales pour Node.js
-      es6: true,  // Ajoute les variables globales pour ES6
-    },
-    // autres configurations
-  };
-  
