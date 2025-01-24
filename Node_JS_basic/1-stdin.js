5e599438e9dea33025ace1501bd010b24cf6c1e0
hziri54 <zirihouamdi6'@gmail.com>
2025-01-24 09:45:46 +0100

    task 1 finish

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -3,10 +3,6 @@ console.log('Welcome to Holberton School, what is your name?');
 process.stdin.on('data', (input) => {
   const name = input.toString().trim();
   console.log(`Your name is: ${name}`);
-
-
   console.log('This important software is now closing');
-
-  
   process.exit();
 });
