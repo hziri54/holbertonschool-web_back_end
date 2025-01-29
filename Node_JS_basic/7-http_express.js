20cd7376010097ea480d009171e59ff3684dbb90
hziri54 <zirihouamdi6'@gmail.com>
2025-01-29 14:38:17 +0100

    task 7 correction

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -1,37 +1,59 @@
-const express = require('express');
-const fs = require('fs');
-const countStudents = require('./3-read_file_async');
+const express = require("express");
+const fs = require("fs");
 
 const app = express();
+const PORT = 1245;
 
-app.get('/', (req, res) => {
-  res.send('Hello Holberton School!');
-});
+const countStudents = (databasePath) => {
+    return new Promise((resolve, reject) => {
+        fs.readFile(databasePath, "utf-8", (err, data) => {
+            if (err) {
+                reject("Cannot load the database");
+                return;
+            }
 
-app.get('/students', (req, res) => {
-  const databasePath = process.argv[2];
+            const lines = data.split("\n").filter(line => line.trim() !== "");
+            const students = {};
+            let totalStudents = 0;
 
-  if (!databasePath) {
-    return res.status(500).send('Database file path not provided');
-  }
+            lines.slice(1).forEach((line) => {
+                const parts = line.split(",");
+                const field = parts[3]; 
+                const firstName = parts[0];
 
-  if (!fs.existsSync(databasePath)) {
-    return res.status(500).send('Cannot load the database');
-  }
+                if (field) {
+                    if (!students[field]) students[field] = [];
+                    students[field].push(firstName);
+                    totalStudents++;
+                }
+            });
 
-  res.write('This is the list of our students\n');
+            let result = `This is the list of our students\nNumber of students: ${totalStudents}`;
+            for (const field in students) {
+                result += `\nNumber of students in ${field}: ${students[field].length}. List: ${students[field].join(", ")}`;
+            }
 
-  countStudents(databasePath)
-    .then(() => {
-      res.end();
-    })
-    .catch((err) => {
-      res.status(500).send(err.message);
+            resolve(result);
+        });
     });
+};
+
+app.get("/", (req, res) => {
+    res.send("Hello Holberton School!");
 });
 
-app.listen(1245, () => {
-  console.log('Server is listening on port 1245');
+app.get("/students", async (req, res) => {
+    const databasePath = process.argv[2]; 
+    try {
+        const studentsList = await countStudents(databasePath);
+        res.send(studentsList);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
 
 module.exports = app;
