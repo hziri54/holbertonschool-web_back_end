341e87abeb6c02b4613e7d6a7d355868e59ecfe5
hziri54 <zirihouamdi6'@gmail.com>
2025-01-30 10:07:02 +0100

    modif

--- python_variable_annotations/3-to_str.py
+++ python_variable_annotations/3-to_str.py
@@ -1,12 +1,3 @@
 #!/usr/bin/env python3
 def to_str(n: float) -> str:
-    """
-    Prend un nombre flottant n et renvoie sa représentation sous forme de chaîne de caractères.
-    
-    Args:
-        n (float): Le nombre flottant à convertir.
-    
-    Returns:
-        str: La chaîne de caractères représentant n.
-    """
     return str(n)
