d2c8a09a52855584e2469efddd1964831d4900e8
hziri54 <zirihouamdi6'@gmail.com>
2025-03-08 16:45:07 +0100

    task 0 finish

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -6,9 +6,9 @@ import typing
 
 async def async_generator() -> typing.Generator[float, None, None]:
     """
-    Boucle 10 fois, qui attend 1 seconde,
-    puis génère un nombre aléatoire qui est entre 0 et 10.
+    Boucle 10 fois, attend 1 seconde de manière asynchrone,
+    puis génère un nombre aléatoire entre 0 et 10.
     """
     for _ in range(10):
-        await asyncio.sleep(1)  
-        yield random.uniform(0, 10)  
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
