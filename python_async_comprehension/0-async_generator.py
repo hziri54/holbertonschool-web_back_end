a5b7ed7c2cf77087fc1cc87f42b21242b885510a
hziri54 <zirihouamdi6'@gmail.com>
2025-03-08 16:49:15 +0100

    task 0 finish

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,20 +1,13 @@
 #!/usr/bin/env python3
-"""
-Ce module contient une coroutine asynchrone qui génère
-des nombres aléatoires entre 0 et 10 avec une pause d'une seconde.
-"""
-
 import asyncio
 import random
+import typing
 
 
 async def async_generator() -> typing.Generator[float, None, None]:
     """
-    Générateur asynchrone qui attend 1 seconde entre chaque itération
-    et retourne un nombre flottant aléatoire entre 0 et 10.
-
-    Retourne :
-        typing.Generator[float, None, None] : Un générateur asynchrone de nombres aléatoires.
+    Boucle 10 fois, attend 1 seconde de manière asynchrone,
+    puis génère un nombre aléatoire entre 0 et 10.
     """
     for _ in range(10):
         await asyncio.sleep(1)
