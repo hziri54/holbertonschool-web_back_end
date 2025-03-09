036bab233c4f7d691fb1a158f69bbcb9d71b7b43
hziri54 <zirihouamdi6'@gmail.com>
2025-03-09 12:16:26 +0100

    task 1 finish

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,30 +1,18 @@
 #!/usr/bin/env python3
-""" 
-Module qui exécute plusieurs appels asynchrones à wait_random 
-et retourne les délais en ordre croissant, sans utiliser sort().
+"""
+Module qui utilise une compréhension async pour récupérer
+10 nombres aléatoires depuis async_generator.
 """
 
-import asyncio
 from typing import List
-wait_random = __import__('0-basic_async_syntax').wait_random
+async_generator = __import__('0-async_generator').async_generator
 
 
-async def wait_n(n: int, max_delay: int) -> List[float]:
+async def async_comprehension() -> List[float]:
     """
-    Lance n fois wait_random avec max_delay et retourne les délais en ordre croissant.
-
-    Args:
-        n (int): Nombre d'exécutions de wait_random.
-        max_delay (int): Délai maximum pour wait_random.
+    Récupère 10 nombres aléatoires en utilisant une compréhension async.
 
     Returns:
-        List[float]: Liste des délais obtenus, triés en ordre croissant.
+        List[float]: Une liste contenant 10 nombres flottants aléatoires.
     """
-    delays = await asyncio.gather(*(wait_random(max_delay) for _ in range(n)))
-
-    sorted_delays = []
-    while delays:
-        min_val = min(delays)
-        sorted_delays.append(min_val)
-        delays.remove(min_val)
-    return sorted_delays
+    return [num async for num in async_generator()]
