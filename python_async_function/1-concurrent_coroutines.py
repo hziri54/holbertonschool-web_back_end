0365c576549a577a6fa9737115638812ec19bf6e
hziri54 <zirihouamdi6'@gmail.com>
2025-03-12 09:32:59 +0100

    task 1 finish

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,17 +1,27 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+"""Execute multiple coroutines concurrently using async.
+
+This module imports the wait_random function from a previous file and
+defines an async function called wait_n. The function spawns wait_random 
+n times with the specified max_delay and returns a list of the delays 
+in ascending order.
+"""
+
 import asyncio
 from typing import List
+wait_r = __import__('0-basic_async_syntax').wait_random
 
-wait_random = __import__('0-basic_async_syntax').wait_random
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    """Spawns wait_random n times with the specified max_delay
-    and returns a list of all the delays in ascending order."""
-    tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
-    delays = []
+    """Run wait_random n times and return a list of the delays.
 
-    for task in asyncio.as_completed(tasks):
-        delay = await task
-        delays.append(delay)
-    
+    Args:
+        n (int): Number of times to call wait_random.
+        max_delay (int): Maximum delay value for wait_random.
+
+    Returns:
+        List[float]: List of delay values in ascending order.
+    """
+    tasks = [asyncio.create_task(wait_r(max_delay)) for _ in range(n)]
+    delays = [await task for task in asyncio.as_completed(tasks)]
     return delays
