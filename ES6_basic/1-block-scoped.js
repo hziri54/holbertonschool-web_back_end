417e1139031c11f626b98017a84f8a8dd0793197
hziri54 <zirihouamdi6'@gmail.com>
2025-03-16 13:03:17 +0100

    Updated Python scripts and cleaned up repository

---
+++ ES6_basic/1-block-scoped.js
@@ -0,0 +1,5 @@
+export default function taskBlock() {
+  const task = false;
+  const task2 = true;
+  return [task, task2];
+}
--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -5,3 +5,4 @@
 def concat(str1: str, str2: str) -> str:
     """Concatène deux chaînes de caractères et retourne le résultat."""
     return str1 + str2
+,
