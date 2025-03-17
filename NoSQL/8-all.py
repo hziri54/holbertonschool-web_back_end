8f09340d5bae4c440b246aec923a72411eb7fadd
hziri54 <zirihouamdi6'@gmail.com>
2025-03-18 00:10:34 +0100

    task 8 finissh

---
+++ NoSQL/8-all.py
@@ -0,0 +1,10 @@
+from pymongo import MongoClient
+
+def list_all(mongo_collection):
+    """
+    Lists all documents in a collection.
+    
+    :param mongo_collection: pymongo collection object
+    :return: list of documents, empty list if no document found
+    """
+    return list(mongo_collection.find())
