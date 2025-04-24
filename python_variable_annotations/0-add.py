843c8711411904e7b0eca218026ad6344650a0b6
hziri54 <zirihouamdi6'@gmail.com>
2025-04-24 14:51:22 +0200

    task 0 finish

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module contenant une fonction add avec des annotations de type."""
+
+
+def add(a: float, b: float) -> float:
+    """Retourne la somme de deux nombres flottants."""
+    return a + b
