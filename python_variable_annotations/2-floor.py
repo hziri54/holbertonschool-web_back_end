8e597521be6e7f62b0f3607cd030e3d5575e6cb8
hziri54 <zirihouamdi6'@gmail.com>
2025-04-24 15:09:32 +0200

    task 2 finish

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Module contenant une fonction floor avec des annotations de type."""
+
+import math
+
+
+def floor(n: float) -> int:
+    """Retourne la partie entière inférieure d'un nombre flottant."""
+    return math.floor(n)
