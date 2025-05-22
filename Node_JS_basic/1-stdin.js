42d94b1530de90243211caa152981c1b2b539d86
hziri54 <zirihouamdi6'@gmail.com>
2025-05-22 10:01:36 +0200

    task 1 correction

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,8 +1,10 @@
-console.log('Welcome to Holberton School, what is your name?');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('data', (input) => {
-  const name = input.toString().trim();
+process.stdin.on('data', (data) => {
+  const name = data.toString().trim();
   console.log(`Your name is: ${name}`);
+});
+
+process.stdin.on('end', () => {
   console.log('This important software is now closing');
-  process.exit();
 });
